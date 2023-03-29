e766cf66d37700d485d07e14e9ce13c58a80d049
Chris Athanas <realityexpander@gmail.com>
2023-03-29 06:36:25 -0500

    Put styles at the end of MCW.css

--- page_home.js
+++ page_home.js
@@ -8,22 +8,35 @@ class Home extends LitElement {
     // If provided, the properties for type and day are taking from the path.
     //const { type = 'NASDAC', day = 'monday' } = this.routeProps;
     return html`
-      This is the page for Home ${this.routeProps}
-      <br>
-      <a href="/">Home</a>
-      <a href="/stocks">Stocks</a>
-      <a href="/trade/123">Trade</a>
-      <a href="/news/tech">News</a>
+      <div style="padding:10px;">
+        This is the page for Home ${this.routeProps}
+        <br>
+        <a href="/">Home</a>
+        <a href="/stocks">Stocks</a>
+        <a href="/trade/123">Trade</a>
+        <a href="/news/tech">News</a>
 
-      <br>
-      <button @click=${() => window.location = '/stocks'}>Go to stocks</button>
-      <br>
-      <button @click=${() => this.logout()}>Log Out</button>
-
-      <button id="btn-send-item" class="mdc-button mdc-button--outlined smaller-text">
-          <div class="mdc-button__ripple"></div>
-          <span class="mdc-button__label">Send Item</span>
-      </button>
+        <br>
+        <button @click=${() => window.location = '/stocks'}>Go to stocks</button>
+        <br>
+        <button @click=${() => this.logout()}>Log Out</button>
+        <br>
+        <br>
+        <button id="btn-send-item" class="mdc-button mdc-button--outlined smaller-text">
+            <div class="mdc-button__ripple"></div>
+            <span class="mdc-button__label">Send Item</span>
+        </button>
+        <br>
+        <br>
+        <label class="mdc-text-field mdc-text-field--filled">
+          <span class="mdc-text-field__ripple"></span>
+          <span class="mdc-floating-label" id="hint-email-address">Email Address</span>
+          <input id="ti-email-address" class="mdc-text-field__input" type="email" aria-labelledby="my-label-id">
+          <span class="mdc-line-ripple"></span>
+        </label>
+        <br>
+        <br>
+      <div>
       `
   }
 
@@ -32,6 +45,20 @@ class Home extends LitElement {
     for (let i = 0, button; button = buttons[i]; i++) {
       mdc.ripple.MDCRipple.attachTo(button);
     }
+
+    /////////////////////////////////////////////////////////////
+    // listen for text changes to Email Address
+    const emailAddressEl = this.shadowRoot.querySelector('#ti-email-address');
+    const emailAddress = mdc.textField.MDCTextField.attachTo(this.shadowRoot.querySelector('.mdc-text-field--filled'));
+    // emailAddressEl.addEventListener('input', (e) => {
+    //   document.querySelector('#ti-text-area').textContent = e.target.value;
+    //   state.email = e.target.value;
+    // });
+    emailAddress.foundation.adapter.registerInputInteractionHandler('input', (e) => {
+      document.querySelector('#ti-text-area').textContent = e.target.value;
+      state.email = e.data;
+    });
+
   }
 
   logout() {
