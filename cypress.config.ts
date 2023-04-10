343133f508c80283499d64e5f0b88288d7adfb58
Chris Athanas <realityexpander@gmail.com>
2023-04-10 03:55:28 -0500

    fix config?

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,18 @@
-export default {
-  projectId: "e51h9h",
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  projectId: 'a7bq2k',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+})
+
+// export default {
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// };
